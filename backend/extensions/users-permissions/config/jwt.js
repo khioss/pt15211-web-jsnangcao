752b04dce1d396e504fb36b45c782896bf8782c9
Dat Le Trong <datlt2306@gmail.com>
2020-11-23 11:09:25 +0700

    update

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74cb8f4d-13e4-493d-9a7d-6cef0820b14c'
+};
---
+++ data.js
@@ -0,0 +1,7 @@
+const product = {
+    id: 1,
+    name: 'Iphone X',
+    image: 'http://lorempixel.com/500/500',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ut voluptate excepturi, accusantium quas optio ea, sequi provident commodi doloribus odio temporibus totam natus ad. Suscipit vitae inventore dignissimos animi?',
+    price: 20.000
+};
